e3fab9a536f5a668b6d28a2f34e9bbd1fd19bbef
thachvietha <thachvietha09092006@gmail.com>
2025-10-03 19:24:41 +0700

    products

---
+++ BE/src/main/java/udpm/hn/server/core/admin/products/modal/request/ProductsRequest.java
@@ -0,0 +1,28 @@
+package udpm.hn.server.core.admin.products.modal.request;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import udpm.hn.server.core.common.base.PageableRequest;
+import udpm.hn.server.entity.Brand;
+import udpm.hn.server.entity.Category;
+import udpm.hn.server.entity.ProductDetail;
+import udpm.hn.server.entity.QuoteItem;
+import udpm.hn.server.infrastructure.core.constant.EntityProperties;
+import udpm.hn.server.infrastructure.core.constant.EntityStatus;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProductsRequest extends PageableRequest {
+         String sku;
+         String name;
+         String brandId;
+         String categoryId;
+
+}
---
+++ BE/src/main/java/udpm/hn/server/core/admin/products/reponsitory/ProductExtendReponsitory.java
@@ -0,0 +1,62 @@
+package udpm.hn.server.core.admin.products.reponsitory;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import udpm.hn.server.core.admin.products.modal.reponse.ProductsReponse;
+import udpm.hn.server.core.admin.products.modal.request.ProductsRequest;
+import udpm.hn.server.repository.ProductRepository;
+@Repository
+public interface ProductExtendReponsitory extends ProductRepository {
+    @Query(
+            value = """
+        SELECT
+           ROW_NUMBER() OVER (ORDER BY p.created_date DESC) AS orderNumber,
+           p.sku AS sku,
+           p.name AS name,
+           p.short_description AS shortDescription,
+           p.price AS price,
+           p.stock_quantity AS stockQuantity,
+           b.name AS brandName,
+           c.name AS categoriesName,
+           pd.id AS productDetailId,
+           i.id AS imagesId,
+           p.created_date AS createdDate
+        FROM catalog_web.products p
+        JOIN catalog_web.product_details pd ON pd.product_id = p.id
+        JOIN catalog_web.categories c ON c.id = p.category_id
+        JOIN catalog_web.brands b ON b.id = p.brand_id
+        JOIN catalog_web.images i ON i.product_detail_id = pd.id
+        WHERE
+           (
+             (:#{#request.sku} IS NULL OR LOWER(p.sku) LIKE CONCAT('%', LOWER(:#{#request.sku}), '%'))
+             OR
+             (:#{#request.name} IS NULL OR LOWER(p.name) LIKE CONCAT('%', LOWER(:#{#request.name}), '%'))
+           )
+           AND (:#{#request.brandId} IS NULL OR b.id = :#{#request.brandId})
+           AND (:#{#request.categoryId} IS NULL OR c.id = :#{#request.categoryId})
+        """,
+            countQuery = """
+        SELECT COUNT(DISTINCT p.id)
+        FROM catalog_web.products p
+        JOIN catalog_web.product_details pd ON pd.product_id = p.id
+        JOIN catalog_web.categories c ON c.id = p.category_id
+        JOIN catalog_web.brands b ON b.id = p.brand_id
+        JOIN catalog_web.images i ON i.product_detail_id = pd.id
+        WHERE
+           (
+             (:#{#request.sku} IS NULL OR LOWER(p.sku) LIKE CONCAT('%', LOWER(:#{#request.sku}), '%'))
+             AND
+             (:#{#request.name} IS NULL OR LOWER(p.name) LIKE CONCAT('%', LOWER(:#{#request.name}), '%'))
+           )
+           AND (:#{#request.brandId} IS NULL OR b.id = :#{#request.brandId})
+           AND (:#{#request.categoryId} IS NULL OR c.id = :#{#request.categoryId})
+        """,
+            nativeQuery = true
+    )
+    Page<ProductsReponse> findAllByFilters(@Param("request") ProductsRequest request, Pageable pageable);
+
+}
