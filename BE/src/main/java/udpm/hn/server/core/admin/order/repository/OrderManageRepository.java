2d1cc7aba5434f1cbd350e75d69d32a23db4ce5d
Cloudn9-k <Tongankhang0407@gmail.com>
2025-12-03 13:24:33 +0700

    Sua order ket noi backend

--- BE/src/main/java/udpm/hn/server/core/admin/order/repository/OrderManageRepository.java
+++ BE/src/main/java/udpm/hn/server/core/admin/order/repository/OrderManageRepository.java
@@ -1,17 +1,11 @@
 package udpm.hn.server.core.admin.order.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import udpm.hn.server.entity.Order;
-import udpm.hn.server.infrastructure.core.constant.EntityStatus;
-import udpm.hn.server.repository.OrderRepository; // (Tạo interface này extends JpaRepository nếu chưa có)
+import udpm.hn.server.repository.OrderRepository;
 
 @Repository
-public interface OrderManageRepository extends OrderRepository {
-    // Lấy danh sách đơn hàng ACTIVE
-    Page<Order> findByStatus(EntityStatus status, Pageable pageable);
-
-    // Tìm theo mã đơn hàng
+public interface OrderManageRepository extends OrderRepository, JpaSpecificationExecutor<Order> {
     boolean existsByOrderCode(String orderCode);
 }
--- BE/src/main/java/udpm/hn/server/core/admin/order/service/OrderService.java
+++ BE/src/main/java/udpm/hn/server/core/admin/order/service/OrderService.java
@@ -2,11 +2,12 @@ package udpm.hn.server.core.admin.order.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import udpm.hn.server.core.admin.order.dto.request.OrderCreateRequest;
+import udpm.hn.server.core.admin.order.dto.request.OrderFilterRequest;
 import udpm.hn.server.core.admin.order.dto.response.OrderResponse;
+import udpm.hn.server.entity.Order;
 
 public interface OrderService {
-    Page<OrderResponse> getAllOrders(Pageable pageable);
+    Page<OrderResponse> getAllOrders(OrderFilterRequest request, Pageable pageable);
     OrderResponse getOrderById(String id);
-    OrderResponse createOrder(OrderCreateRequest request);
+    void updateOrderStatus(String id, Order.OrderStatus newStatus);
 }
