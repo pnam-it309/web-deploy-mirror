44414f49defd3aa4fdddb84377bb490d05ca4c6e
Nam Phương <nickhunter3009@gmail.com>
2025-12-04 22:48:23 +0700

    hoàn thiện customer

--- BE/src/main/java/udpm/hn/server/core/customer/register/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package udpm.hn.server.core.customer.register.service;
-
-import udpm.hn.server.core.admin.order.dto.response.OrderResponse;
-import udpm.hn.server.core.customer.register.dto.request.OrderRequest;
-
-public interface OrderService {
-    OrderResponse createOrder(OrderRequest orderRequest);
-}
